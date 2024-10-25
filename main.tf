3c63abbaf432481b8b9f799f90ce5cf7d295ea0d
Test-usr-17 <test.akhin98@gmail.com>
2024-10-25 00:05:14 +0000

    Security Group For Monitoring Ec2 Created

--- main.tf
+++ main.tf
@@ -10,6 +10,38 @@ resource "aws_key_pair" "ssh_keypair" {
 }
 
 
+resource "aws_security_group" "monitoring" {
+
+  name        = "${var.project_name}-${var.project_environment}-monitoring"
+  description = "${var.project_name}-${var.project_environment}-monitoring"
+  vpc_id      = var.default_vpc_id
+  tags = {
+    Name = "${var.project_name}-${var.project_environment}-monitoring"
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_security_group_rule" "monitoring_ingress_rule" {
+
+  for_each = toset(var.monitoring_ports)
+
+  type              = "ingress"
+  from_port         = each.key
+  to_port           = each.key
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+  security_group_id = aws_security_group.monitoring.id
+}
+
+
 # Creating Frontend Security group
 resource "aws_security_group" "frontend" {
 
