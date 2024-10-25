3ebf5db19dd29ed63ec3074482b08c78a68bc9a5
Revenant07 <akhinauchu@gmail.com>
2024-10-25 00:37:31 +0000

    Monitoring PublicIP output

--- output.tf
+++ output.tf
@@ -2,6 +2,11 @@ output "frontend_instance_id" {
   value = aws_instance.frontend.id
 }
 
+output "monitoring_instance_id" {
+  value = aws_instance.monitoring.id
+}
+
+
 output "frontend_public_name" {
   value = "http://${var.hostname}.${var.domain_name}"
 }
